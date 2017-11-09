c0db76676033941ab06b5f08732be166bd050622
Rimil Dey <rimildeyjsr@gmail.com>
2017-11-09 15:34:41 +0530

    initial and final commit

--- SetLocation/ViewController.swift
+++ SetLocation/ViewController.swift
@@ -7,19 +7,65 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    // MARK: - view did load
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: - outlets
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    // MARK: - interactions
+    
+    @IBAction func tapSetLocationButton(_ sender: UIButton) {
+        getLocation()
     }
+    
+    // MARK: - core location
+    let locationManager = CLLocationManager()
 
+    // MARK:  location functions
+    
+    func getLocation() {
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+        locationManager.startUpdatingLocation()
+    }
+    
+    // MARK:  CLLocation Manager Delegate
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let geoCoder = CLGeocoder()
+        
+        geoCoder.reverseGeocodeLocation(manager.location!) { (placemarks, error) in
+            if let placemarksData = placemarks {
+                let locationData = placemarksData[0]
+                
+                let city = locationData.locality!
+                let state = locationData.administrativeArea!
+                let zipCode = locationData.postalCode!
+                let country = locationData.country!
+                let location = "\(city), \(state), \(zipCode), \(country)"
+                
+                self.locationLabel.text = location
+                self.locationLabel.textColor = .black
+                
+                self.locationManager.stopUpdatingLocation()
+
+            } else {
+                print("Error: \(String(describing: error?.localizedDescription))")
+            }
+        }
+    }
+    
 
 }
 
